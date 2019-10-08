275d8a42bb8d341f0166fb2fb8e2ef32885578da
AmitNiloy <amitniloy95@gmail.com>
2019-10-08 12:35:41 +0600

    first commit

---
+++ studentmanagement/resources/views/layouts/includes/siderbar.blade.php
@@ -0,0 +1,23 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">Sidebar</div>
+        <div class="card-body">
+            <a href="{{ url('departments') }}">Departments</a>
+        </div>
+        <div class="card-body">
+            <a href="{{ url('classes') }}">Class</a>
+        </div>
+        <div class="card-body">
+            <a href="{{ url('students') }}">Students</a>
+        </div>
+    </div>
+</div>
+
+{{--
+<ul class="list-group">
+    <li class="list-group-item active">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Morbi leo risus</li>
+    <li class="list-group-item">Porta ac consectetur ac</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+</ul>--}}
